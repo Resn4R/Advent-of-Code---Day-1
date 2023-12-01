3363f6e9a49643f240666885153f6bbaacb7e614
Resn4R <purview-joust.0x@icloud.com>
2023-12-01 10:51:20 +0000

    Initial Commit

---
+++ Advent of Code - Day 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Advent of Code - Day 1
+//
+//  Created by Vito Borghi on 01/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
